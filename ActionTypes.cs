632f97c18ec10c209fb06f6789561e371ee393c6
Emem Isaac <me@ememisaac.com>
2022-05-31 22:45:12 +0100

    Adds the list of recognized request event types

---
+++ ActionTypes.cs
@@ -0,0 +1,12 @@
+namespace WorkflowEngine
+{
+
+    /// <summary>
+    /// List of different types of actions that may happen to a request
+    /// </summary>
+    public enum ActionType
+    {
+        FirstApproval,
+        SecondApproval
+    }
+}
